492816240c0a0f738e7deffab6c9482e4fbe6282
LenarGilmanov <lagilmanovla@gmail.com>
2024-12-10 20:27:30 +0300

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World!\n');
+  res.status(200).send(' Hello, World!\n');
 });
 module.exports = app;
